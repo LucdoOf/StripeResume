b7f4f50e98ee924d5b95ec663ad4e7d02f923f2b
lucas <lucasg.18@hotmail.fr>
2020-09-17 23:31:33 +0200

    Editor cleaning up

--- views/home-editor.php
+++ views/home-editor.php
@@ -1,30 +1,30 @@
 <section id="editor">
     const experiences = {
-    database_administration: {
-    duration: "January 2015 - February 2015",
-    business: "Mediacoms",
-    place: "18000 Bourges",
-    description: "Renewing and cleaning client's SQL database",
-    },
-    stage_manager: {
-    duration: "November 2017, November 2018, November 2019",
-    business: "Le festival de la guitare Issoudun",
-    place: "36100 Issoudun France",
-    description: "Sound and image management for a temporal radio and for streaming on internet"
-    },
-    web_developer: {
-    duration: "May - August 2019, May - August 2020",
-    business: "Boreal Business",
-    place: "18000 Bourges France",
-    description: "Web development specialized in insurances (first time using Stripe !)"
-    },
-    auto_entrepreneur: {
-    duration: "Since March 2020",
-    business: "Agile-Web.net",
-    place: "37100 Tours France",
-    description: "Creating e-commerce websites using Stripe, PHP and VueJS",
-    business_website: "https://agile-web.net",
-    ecommerce_dashboard_example: "https://demo.agile-web.dev"
-    }
+        database_administration: {
+            duration: "January 2015 - February 2015",
+            business: "Mediacoms",
+            place: "18000 Bourges",
+            description: "Renewing and cleaning client's SQL database",
+        },
+        stage_manager: {
+            duration: "November 2017, November 2018, November 2019",
+            business: "Le festival de la guitare Issoudun",
+            place: "36100 Issoudun France",
+            description: "Sound and image management for a webradio and for an Hertzian radio"
+        },
+        web_developer: {
+            duration: "May - August 2019, May - August 2020",
+            business: "Boreal Business",
+            place: "18000 Bourges France",
+            description: "Web development specialized in insurances (first time using Stripe !)"
+        },
+        auto_entrepreneur: {
+            duration: "Since March 2020",
+            business: "Agile-Web.net",
+            place: "37100 Tours France",
+            description: "Creating e-commerce websites using Stripe, PHP and VueJS",
+            business_website: "https://agile-web.net",
+            ecommerce_dashboard_example: "https://demo.agile-web.dev"
+        }
     };
 </section>
