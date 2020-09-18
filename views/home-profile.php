1675cd1e85cfabc1107b0a19f02da45aca0ccb08
lucas <lucasg.18@hotmail.fr>
2020-09-18 11:51:01 +0200

    Wording

--- views/home-profile.php
+++ views/home-profile.php
@@ -5,12 +5,12 @@
         Currently in 3rd year of my engineer diploma, I'm looking <strong>for a 3-months internship for the 2021 summer.</strong>
         I have always been passionate by computer sciences, and <strong>I develop for more than 6 years now.</strong> For about 3 years, I work every summer
         for web companies to help them during employees breaks. <strong>I mainly work on PHP back-end</strong>, which is what I prefer, but front-end is a part of my skills as well.
-        Moreover, I recently started my own business where I realize e-commerce websites for professionals.
+        <br/>Moreover, I recently started my own business where I realize e-commerce websites for professionals.
         I'm supplying to the Stripe Company because the <strong>Stripe web solutions have always been a model and a part of my work</strong>.
         In fact, <strong>I work with Stripe tools for about 2 years.</strong> I implemented Stripe billing, Stripe invoices, Stripe payments with payments intents or Stripe payment
         links.
         It has always been an evidence to use Stripe in my payment processes, and I always enjoyed using it.
-        Also, the stripe's front website and the stripe's dashboard have inspired me a lot in terms of UI and UX for my personal and professional projects.
+        <br/>Also, the stripe's front website and the stripe's dashboard have inspired me a lot in terms of UI and UX for my personal and professional projects.
         That's why <strong>It would be for me an honor and a pleasure</strong> to work for Stripe and to contribute to making payments a lot easier across the world.
     </p>
 </section>
